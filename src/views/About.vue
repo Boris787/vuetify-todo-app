c6fdb1f8bf18b6938f6e07eb4018b80bc0f71231
Boris787 <borismrdja78@gmail.com>
2021-02-15 20:33:57 +0100

    Finished/alnapo Vuetify todo app la

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todos</h1>
+    <p>Lorem ipsum dolor sit amet, consectuter adispicing elit. Sed feugiat justo id viverra consequat. Integer feugiat lorem faucibus est ornare scelerisque. Donec tempus, nunc vitae semper sagitts, odio manga semper ipsum, et laoreet sapien mauris vitar arcu.</p>
+    <p>Lorem ipsum dolor sit amet, consectuter adispicing elit. Sed feugiat justo id viverra consequat. Integer feugiat lorem faucibus est ornare scelerisque. Donec tempus, nunc vitae semper sagitts, odio manga semper ipsum, et laoreet sapien mauris vitar arcu.</p>
+    <p>Lorem ipsum dolor sit amet, consectuter adispicing elit. Sed feugiat justo id viverra consequat. Integer feugiat lorem faucibus est ornare scelerisque. Donec tempus, nunc vitae semper sagitts, odio manga semper ipsum, et laoreet sapien mauris vitar arcu.</p>
   </div>
 </template>
